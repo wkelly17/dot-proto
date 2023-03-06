c217cd1f791f9cf071505ac9947a80da659dfba9
Will Kelly <willkelly@Will-Kellys-Pro.local>
2023-03-06 14:56:31 -0600

    trying to fix6

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -9,7 +9,7 @@ import {
   setAllVideos,
 } from "../store";
 
-import videojsPlaylistPlugin from "videojs-playlist";
+// import videojsPlaylistPlugin from "videojs-playlist";
 import {getMp4DownloadSize} from "../utils";
 
 export function VideoPlayer(props: any) {
@@ -27,96 +27,96 @@ export function VideoPlayer(props: any) {
     };
   });
 
-  onMount(() => {
-    let el = document.querySelector("#vid1");
+  // onMount(() => {
+  //   let el = document.querySelector("#vid1");
 
-    if (el) {
-      window.videojs(el);
-    }
-  });
-  createEffect(async () => {
-    // if (!playerSrc()) return;
-    // let el = document.querySelector("#videoPlayerDiv");
-    let el = document.querySelector("#videoPlayer");
-    if (!el && !player()) return;
-    if (!player()) {
-      const options = {
-        responsive: true,
-        fluid: true,
-        controls: true,
-        playbackRates: [0.5, 1, 1.5, 2, 2.5, 3],
-      };
-      // const playerInstance = await brightcovePlayerLoader({
-      //   refNode: el,
-      //   refNodeInsert: "append",
-      //   accountId: props.accountId,
-      //   playerId: "9mlrvmAybr",
-      //   controls: true,
-      //   // embedType: "iframe",
-      //   options
-      //   // playlistId: "ref:benin-new-testament",
-      //   // embedOptions: {
-      //   //   playlist: true,
-      //   //   responsive: true,
-      //   // },
+  //   if (el) {
+  //     window.videojs(el);
+  //   }
+  // });
+  // createEffect(async () => {
+  //   // if (!playerSrc()) return;
+  //   // let el = document.querySelector("#videoPlayerDiv");
+  //   let el = document.querySelector("#videoPlayer");
+  //   if (!el && !player()) return;
+  //   if (!player()) {
+  //     const options = {
+  //       responsive: true,
+  //       fluid: true,
+  //       controls: true,
+  //       playbackRates: [0.5, 1, 1.5, 2, 2.5, 3],
+  //     };
+  //     // const playerInstance = await brightcovePlayerLoader({
+  //     //   refNode: el,
+  //     //   refNodeInsert: "append",
+  //     //   accountId: props.accountId,
+  //     //   playerId: "9mlrvmAybr",
+  //     //   controls: true,
+  //     //   // embedType: "iframe",
+  //     //   options
+  //     //   // playlistId: "ref:benin-new-testament",
+  //     //   // embedOptions: {
+  //     //   //   playlist: true,
+  //     //   //   responsive: true,
+  //     //   // },
 
-      //   // playlistId: "ref:1745043212224883810",
-      // });
-      const playerInstance = window.videojs(el, options);
-      window.videojs.registerPlugin("playlist", videojsPlaylistPlugin);
-      playerInstance.playlist(sources);
-      // playerInstance.ref.on("pause", () => {
-      //   console.log("PUASE");
-      // });
-      setPlayer(playerInstance);
-      const mp4 = sources[0].sources?.find((src) => {
-        return src && src.container == "MP4";
-      });
-      // 2test.zip?src=${href}&names=${namesHref}&size=${size}
-      const encoded = `2test.zip?src=${encodeURIComponent(
-        JSON.stringify([mp4.src])
-      )}&names=${encodeURIComponent(JSON.stringify([sources[0].slug]))}&size=${
-        sources[0].size
-      }`;
-      setMp4Src(encoded);
-      // playerInstance.ref.playlist(sources);
-      // playerInstance.ref.on("pause", () => {
-      //   console.log("PUASE");
-      // });
-    } else {
-      let currentPlayer: any = player();
-      if (!playerSrc() && playerSrc() != 0) return;
-      if (!currentPlayer?.paused) {
-        currentPlayer.pause();
-      }
+  //     //   // playlistId: "ref:1745043212224883810",
+  //     // });
+  //     const playerInstance = window.videojs(el, options);
+  //     window.videojs.registerPlugin("playlist", videojsPlaylistPlugin);
+  //     playerInstance.playlist(sources);
+  //     // playerInstance.ref.on("pause", () => {
+  //     //   console.log("PUASE");
+  //     // });
+  //     setPlayer(playerInstance);
+  //     const mp4 = sources[0].sources?.find((src) => {
+  //       return src && src.container == "MP4";
+  //     });
+  //     // 2test.zip?src=${href}&names=${namesHref}&size=${size}
+  //     const encoded = `2test.zip?src=${encodeURIComponent(
+  //       JSON.stringify([mp4.src])
+  //     )}&names=${encodeURIComponent(JSON.stringify([sources[0].slug]))}&size=${
+  //       sources[0].size
+  //     }`;
+  //     setMp4Src(encoded);
+  //     // playerInstance.ref.playlist(sources);
+  //     // playerInstance.ref.on("pause", () => {
+  //     //   console.log("PUASE");
+  //     // });
+  //   } else {
+  //     let currentPlayer: any = player();
+  //     if (!playerSrc() && playerSrc() != 0) return;
+  //     if (!currentPlayer?.paused) {
+  //       currentPlayer.pause();
+  //     }
 
-      const newSrc = playerSrc();
-      console.log({newSrc});
-      currentPlayer?.playlist.currentItem(newSrc);
-      console.log(currentPlayer.currentSrc());
-      console.log(currentPlayer.currentType());
-      const corresponding = sources[newSrc];
-      console.log({corresponding});
-      const mp4 = corresponding.sources?.find((src) => {
-        return src && src.container == "MP4";
-      });
-      // debugger;
-      const encoded = `2test.zip?src=${encodeURIComponent(
-        JSON.stringify([mp4.src])
-      )}&names=${corresponding.slug}&size=${corresponding.size}`;
-      setMp4Src(encoded);
+  //     const newSrc = playerSrc();
+  //     console.log({newSrc});
+  //     currentPlayer?.playlist.currentItem(newSrc);
+  //     console.log(currentPlayer.currentSrc());
+  //     console.log(currentPlayer.currentType());
+  //     const corresponding = sources[newSrc];
+  //     console.log({corresponding});
+  //     const mp4 = corresponding.sources?.find((src) => {
+  //       return src && src.container == "MP4";
+  //     });
+  //     // debugger;
+  //     const encoded = `2test.zip?src=${encodeURIComponent(
+  //       JSON.stringify([mp4.src])
+  //     )}&names=${corresponding.slug}&size=${corresponding.size}`;
+  //     setMp4Src(encoded);
 
-      // currentPlayer.load();
-      //  currentPlayer.play();
-      // }
-      // const thePlayer = player();
-      // player.src = playerSrc()?.src;
-      // player.on("ready", function () {
-      //   debugger;
-      //   player.src = playerSrc()?.src;
-      // });
-    }
-  });
+  //     // currentPlayer.load();
+  //     //  currentPlayer.play();
+  //     // }
+  //     // const thePlayer = player();
+  //     // player.src = playerSrc()?.src;
+  //     // player.on("ready", function () {
+  //     //   debugger;
+  //     //   player.src = playerSrc()?.src;
+  //     // });
+  //   }
+  // });
 
   return (
     // <div data-vjs-player class="">
